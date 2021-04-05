fc33d03d0088db0395677cad97c4662fbc6eee62
Matthew Taylor <hacks420chickens@outlook.com>
2021-04-05 22:29:12 +0100

    Added NoBoss
    This will disable rendering of boss bars, fog and dark sky.  I figured that might be useful when going through spawn on 2b2t

---
+++ src/main/java/net/como/client/cheats/NoBoss.java
@@ -0,0 +1,31 @@
+package net.como.client.cheats;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.como.client.structures.Cheat;
+
+public class NoBoss extends Cheat {
+    public NoBoss() {
+        super("NoBoss");
+    }
+
+    public void recieveEvent(String eventName, Object[] args) {
+        switch (eventName) {
+            case "onBossBarHudRender": {
+                // Stop the boss bars being rendered
+                CallbackInfo ci = (CallbackInfo)args[0];
+
+                ci.cancel();
+                break;
+            }
+            case "onBossBarHudSkyEffects": {
+                // Stop the sky and what not being changed.
+                CallbackInfoReturnable<Boolean> cir = (CallbackInfoReturnable<Boolean>)args[0];
+                cir.setReturnValue(false);
+                
+                break;
+            }
+        }
+    }
+}
