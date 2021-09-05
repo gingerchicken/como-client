2807bd6f673bd6211c91b101acd27b7231e9f395
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-05 12:35:09 +0100

    Resolve #13

---
+++ src/main/java/net/como/client/cheats/FullBright.java
@@ -0,0 +1,30 @@
+package net.como.client.cheats;
+
+import net.como.client.CheatClient;
+import net.como.client.structures.Cheat;
+import net.minecraft.client.MinecraftClient;
+
+public class FullBright extends Cheat {
+    private Double normalGamma = 0d;
+
+    public FullBright() {
+        super("FullBright");
+        
+        this.description = "Allows you to see anywhere as if it was day.";
+    }
+    
+    @Override
+    public void activate() {
+        MinecraftClient client = CheatClient.getClient();
+
+        this.normalGamma = client.options.gamma;
+        client.options.gamma = 16;
+    }
+
+    @Override
+    public void deactivate() {
+        MinecraftClient client = CheatClient.getClient();
+
+        client.options.gamma = this.normalGamma;
+    }
+}
