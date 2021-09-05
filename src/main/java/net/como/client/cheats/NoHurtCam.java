a8fe72297742825354cded69b5d1c032f2fb4cc3
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-05 01:12:29 +0100

    Added NoHurtCam (for real)
    I made a mistake when committing last time, I forgot to actually add the new files.

---
+++ src/main/java/net/como/client/cheats/NoHurtCam.java
@@ -0,0 +1,25 @@
+package net.como.client.cheats;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.structures.Cheat;
+
+public class NoHurtCam extends Cheat {
+    public NoHurtCam() {
+        super("NoHurtCam");
+
+        this.description = "Disables the screen rotation when getting damaged.";
+    }
+
+    public void recieveEvent(String event, Object[] args) {
+        switch (event) {
+            case "onBobViewWhenHurt": {
+                CallbackInfo ci = (CallbackInfo)args[2];
+
+                ci.cancel();
+
+                break;
+            }
+        }
+    }
+}
