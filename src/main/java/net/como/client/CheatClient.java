acf408af4850de1c6ae14f96b74ecc6a2ba36bb2
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-07 23:46:23 +0100

    Resolve #20
    Added AutoRespawn

--- src/main/java/net/como/client/CheatClient.java
+++ src/main/java/net/como/client/CheatClient.java
@@ -54,6 +54,7 @@ public class CheatClient {
         Cheats.put("nohurtcam", new NoHurtCam());
         Cheats.put("fullbright", new FullBright());
         Cheats.put("autoreconnect", new AutoReconnect());
+        Cheats.put("autorespawn", new AutoRespawn());
 
         registerCheatCommands();
     }
