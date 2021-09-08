8846149315150d38a6a5e965bdb858427f828bd6
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-09 00:28:49 +0100

    Renamed InGameHubRenderEvent to InGameHudRenderEvent

--- src/main/java/net/como/client/events/InGameHudRenderEvent.java
+++ src/main/java/net/como/client/events/InGameHudRenderEvent.java
@@ -5,12 +5,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.como.client.structures.events.Event;
 import net.minecraft.client.util.math.MatrixStack;
 
-public class InGameHubRenderEvent extends Event {
+public class InGameHudRenderEvent extends Event {
     public MatrixStack mStack;
     public float tickDelta;
     public CallbackInfo ci;
 
-    public InGameHubRenderEvent(MatrixStack mStack, float tickDelta, CallbackInfo ci) {
+    public InGameHudRenderEvent(MatrixStack mStack, float tickDelta, CallbackInfo ci) {
         this.mStack = mStack;
         this.tickDelta = tickDelta;
         this.ci = ci;
