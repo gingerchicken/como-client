2d0c1507c5693184e72053c6e04f478610287a22
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-09 19:46:37 +0100

    Removed unused events after #25
    After fixing this there are events for things I am most likely never going to use again, such as rendering the weather (which we can bypass seperately) and rain splash rendering.

--- src/main/java/net/como/client/mixin/WorldRendererMixin.java
+++ src/main/java/net/como/client/mixin/WorldRendererMixin.java
@@ -7,9 +7,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.como.client.CheatClient;
 import net.como.client.events.RenderEntityEvent;
-import net.como.client.events.RenderWeatherEvent;
 import net.como.client.events.RenderWorldEvent;
-import net.como.client.events.TickRainSplashingEvent;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.LightmapTextureManager;
@@ -29,22 +27,4 @@ public class WorldRendererMixin {
     public void onRender(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager arg4, Matrix4f arg5, CallbackInfo ci) {
         CheatClient.emitter.triggerEvent(new RenderWorldEvent(matrices, tickDelta, limitTime, renderBlockOutline, camera, gameRenderer, arg4, arg5, ci));
     }
-
-    @Inject(at = @At("HEAD"), method="renderWeather(Lnet/minecraft/client/render/LightmapTextureManager;FDDD)V", cancellable = true)
-    private void onRenderWeather(LightmapTextureManager manager, float f, double d, double e, double g, CallbackInfo ci) {
-        CheatClient.emitter.triggerEvent(new RenderWeatherEvent(manager, f, d, e, g, ci));
-    }
-    
-    @Inject(at = @At("HEAD"), method="tickRainSplashing(Lnet/minecraft/client/render/Camera;)V", cancellable = true)
-    private void onTickRainSplashing(Camera camera, CallbackInfo ci) {
-        CheatClient.emitter.triggerEvent(new TickRainSplashingEvent(camera, ci));
-    }
-
-    // TODO maybe use this?
-    // @Inject(at = @At("HEAD"), method="renderDarkSky()V", cancellable = true)
-    // private void onRenderDarkSky(CallbackInfo ci) {
-    //     CheatClient.triggerAllEvent("onRenderDarkSky", new Object[] {
-    //         ci
-    //     });
-    // }
 }
