2e6dcb46a500dbca6252ba924411135cf5a9919a
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-09 14:00:19 +0100

    Hooked the RenderFireOverlay event

---
+++ src/main/java/net/como/client/mixin/InGameOverlayRenderMixin.java
@@ -0,0 +1,20 @@
+package net.como.client.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.CheatClient;
+import net.como.client.events.RenderFireOverlayEvent;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+
+@Mixin(InGameOverlayRenderer.class)
+public class InGameOverlayRenderMixin {
+    @Inject(at = @At("HEAD"), method = "renderFireOverlay(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/util/math/MatrixStack;)V", cancellable = true)
+    private static void renderFireOverlay(MinecraftClient client, MatrixStack matrices, CallbackInfo ci) {
+        CheatClient.emitter.triggerEvent(new RenderFireOverlayEvent(client, matrices, ci));
+    }
+}
