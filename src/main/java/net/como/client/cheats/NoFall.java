d263cfa38ab05873a362a89e312213abc92f4605
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-09 12:49:53 +0100

    Resolved #28

--- src/main/java/net/como/client/cheats/NoFall.java
+++ src/main/java/net/como/client/cheats/NoFall.java
@@ -4,6 +4,7 @@ import net.como.client.CheatClient;
 import net.como.client.events.MovementPacketEvent;
 import net.como.client.structures.Cheat;
 import net.como.client.structures.events.Event;
+import net.como.client.utils.ClientUtils;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
@@ -34,8 +35,13 @@ public class NoFall extends Cheat {
                 // Use to make sure that flight is less gittery
                 if(player.fallDistance <= (player.isFallFlying() ? 1 : 2)) break;
 
-                // Make sure that the player is not falling too quickly
-                if(player.isFallFlying() && player.isSneaking() && player.getVelocity().getY() < -0.5) break;
+                if (player.isFallFlying()) {
+                    // Make sure that the player is not falling too quickly
+                    if(player.isSneaking() && player.getVelocity().getY() < -0.5) break;
+
+                    // Make sure that the player is not elytra flying
+                    if (ClientUtils.hasElytraEquipt()) break;
+                }
                 
                 // I believe this just says to the server "ay yo, I am on floor dw 'bout it sweet cheeks :3"
                 player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
