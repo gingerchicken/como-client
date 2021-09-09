fa1ffdfe585ee911abcf787ab88ec7244511f3e9
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-09 19:41:16 +0100

    Resolved #25

--- src/main/java/net/como/client/cheats/NoWeather.java
+++ src/main/java/net/como/client/cheats/NoWeather.java
@@ -1,7 +1,6 @@
 package net.como.client.cheats;
 
-import net.como.client.events.RenderWeatherEvent;
-import net.como.client.events.TickRainSplashingEvent;
+import net.como.client.events.GetRainGradientEvent;
 import net.como.client.structures.Cheat;
 import net.como.client.structures.events.Event;
 
@@ -14,25 +13,19 @@ public class NoWeather extends Cheat {
 
     @Override
     public void activate() {
-        this.addListen(RenderWeatherEvent.class);
-        this.addListen(TickRainSplashingEvent.class);
+        this.addListen(GetRainGradientEvent.class);
     }
 
     @Override
     public void deactivate() {
-        this.removeListen(RenderWeatherEvent.class);
-        this.addListen(TickRainSplashingEvent.class);
+        this.removeListen(GetRainGradientEvent.class);
     }
 
     @Override
     public void fireEvent(Event event) {
         switch (event.getClass().getSimpleName()) {
-            case "RenderWeatherEvent": {
-                ((RenderWeatherEvent)event).ci.cancel();
-                break;
-            }
-            case "TickRainSplashingEvent": {
-                ((TickRainSplashingEvent)event).ci.cancel();
+            case "GetRainGradientEvent": {
+                ((GetRainGradientEvent)event).cir.setReturnValue(0f);
                 break;
             }
         }
