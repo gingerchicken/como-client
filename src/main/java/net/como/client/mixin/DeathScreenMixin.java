abf038a429b41e01ef62837fc5048431c5407a38
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-09 01:15:09 +0100

    Hooked new events with their corresponding mixins

--- src/main/java/net/como/client/mixin/DeathScreenMixin.java
+++ src/main/java/net/como/client/mixin/DeathScreenMixin.java
@@ -6,14 +6,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.como.client.CheatClient;
+import net.como.client.events.DeathEvent;
 import net.minecraft.client.gui.screen.DeathScreen;
 
 @Mixin(DeathScreen.class)
 public class DeathScreenMixin {
     @Inject(at = @At("TAIL"), method = "init()V", cancellable = false)
     public void init(CallbackInfo ci) {
-        CheatClient.triggerAllEvent("onDeath", new Object[] {
-            ci
-        });
+        CheatClient.emitter.triggerEvent(new DeathEvent(ci));
     }
 }
