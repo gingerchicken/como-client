0ebb2523ee69e915905d2711c6c63c4445a217d6
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-12 17:05:44 +0100

    Hooked OnAttackEntityEvent

---
+++ src/main/java/net/como/client/mixin/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,20 @@
+package net.como.client.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.CheatClient;
+import net.como.client.events.OnAttackEntityEvent;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public class ClientPlayerInteractionManagerMixin {
+    @Inject(at = @At("HEAD"), method = "attackEntity(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/entity/Entity;)V", cancellable = true)
+    private void onAttackEntity(PlayerEntity player, Entity target, CallbackInfo ci) {
+        CheatClient.emitter.triggerEvent(new OnAttackEntityEvent(player, target, ci));
+    }    
+}
