037b3c76b3eb1fef0822cc932a146e7b9f4fa62d
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-12 17:05:13 +0100

    Added OnAttackEntityEvent
    Start of #22

---
+++ src/main/java/net/como/client/events/OnAttackEntityEvent.java
@@ -0,0 +1,19 @@
+package net.como.client.events;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.structures.events.Event;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class OnAttackEntityEvent extends Event {
+    public PlayerEntity player;
+    public Entity target;
+    public CallbackInfo ci;
+
+    public OnAttackEntityEvent(PlayerEntity player, Entity target, CallbackInfo ci) {
+        this.player = player;
+        this.target = target;
+        this.ci = ci;
+    }
+}
