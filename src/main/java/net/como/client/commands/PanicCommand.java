8b1cfc9e3bb63bfec4ec422dea0bf713e6e69e90
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-09-16 20:33:46 +0100

    Resolved #45

---
+++ src/main/java/net/como/client/commands/PanicCommand.java
@@ -0,0 +1,31 @@
+package net.como.client.commands;
+
+import net.como.client.CheatClient;
+import net.como.client.commands.structures.Command;
+import net.como.client.structures.Cheat;
+
+public class PanicCommand extends Command {
+
+    public PanicCommand() {
+        super("panic", "panic", "Disables all of your active mods.");
+
+        this.commandDisplay = "Panic";
+    }
+
+    @Override
+    public Boolean trigger(String[] args) {
+        // Make sure that the user knows what they are doing.
+        if (args.length == 0 || !args[0].toLowerCase().startsWith("y")) {
+            this.displayChatMessage("Are you sure you want to disable all your mods?");
+            this.displayChatMessage("Usage: panic <y/N>");
+            return true;
+        }
+
+        // Disable all
+        for (Cheat cheat : CheatClient.Cheats.values()) {
+            if (cheat.isEnabled()) cheat.disable();
+        }
+        this.displayChatMessage("All mods have been disabled.");
+        return true;
+    }
+}
