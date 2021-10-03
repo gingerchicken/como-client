3f1996c18fded4a255bfe12bb9f043d844656255
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-03 16:59:19 +0100

    Resolved #51

--- src/main/java/net/como/client/cheats/Waypoints.java
+++ src/main/java/net/como/client/cheats/Waypoints.java
@@ -1,6 +1,7 @@
 package net.como.client.cheats;
 
 import net.como.client.events.OnRenderEvent;
+import net.como.client.events.RenderWorldViewBobbingEvent;
 import net.como.client.structures.Cheat;
 import net.como.client.structures.WaypointSystem;
 import net.como.client.structures.WaypointSystem.Waypoint;
@@ -17,24 +18,39 @@ public class Waypoints extends Cheat {
     @Override
     public void activate() {
         this.addListen(OnRenderEvent.class);
+        this.addListen(RenderWorldViewBobbingEvent.class);
     }
 
     @Override
     public void deactivate() {
         this.removeListen(OnRenderEvent.class);
+        this.removeListen(RenderWorldViewBobbingEvent.class);
     }
+
+    private boolean isActive = false;
 
     @Override
     public void fireEvent(Event event) {
         switch (event.getClass().getSimpleName()) {
+            case "RenderWorldViewBobbingEvent": {
+                if (!this.isActive) return;
+
+                // Stabilise the tracers
+                RenderWorldViewBobbingEvent e = (RenderWorldViewBobbingEvent)event;
+                e.cancel = true;
+
+                break;
+            }
             case "OnRenderEvent": {
                 OnRenderEvent e = (OnRenderEvent)event;
 
+                this.isActive = false;
                 for (String name : this.waypoints.getWaypoints()) {
                     Waypoint waypoint = this.waypoints.getWaypoint(name);
                     if (!waypoint.enabled) continue;
 
                     RenderUtils.drawTracer(e.mStack, waypoint.pos, e.tickDelta);
+                    this.isActive = true;
                 }
 
                 break;
