d8ee9661324cbc3d4eaefd57f0d3526bc349fc65
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-05 21:04:22 +0100

    Added "storage tracers"
    Technically, it is a ticker esp but whatever.

--- src/main/java/net/como/client/cheats/Tracers.java
+++ src/main/java/net/como/client/cheats/Tracers.java
@@ -1,17 +1,24 @@
 package net.como.client.cheats;
 
+import java.util.HashMap;
+import java.util.List;
+
 import net.como.client.CheatClient;
 import net.como.client.events.OnRenderEvent;
 import net.como.client.events.RenderWorldViewBobbingEvent;
+import net.como.client.interfaces.mixin.IWorld;
 import net.como.client.structures.Cheat;
 import net.como.client.structures.events.Event;
 import net.como.client.structures.settings.Setting;
+import net.como.client.utils.BlockUtils;
 import net.como.client.utils.MathsUtils;
 import net.como.client.utils.RenderUtils;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.vehicle.ChestMinecartEntity;
+import net.minecraft.world.chunk.BlockEntityTickInvoker;
 
 public class Tracers extends Cheat {
 
@@ -23,8 +30,12 @@ public class Tracers extends Cheat {
         this.addSetting(new Setting("Item", true));
         this.addSetting(new Setting("OtherEntities", true));
         
-        // TODO add this
-        // this.addSetting(new Setting("Search", false));
+        // TODO add block search
+        // TODO add entity search mode
+
+        // Block/Ticker search
+        this.addSetting(new Setting("Search", false));
+        this.addSetting(new Setting("SearchPhrases", new HashMap<String, Boolean>()));
     }
     
     @Override
@@ -45,9 +56,9 @@ public class Tracers extends Cheat {
         boolean drawItem          = (boolean)this.getSetting("Item").value;
         boolean drawOtherEntities = (boolean)this.getSetting("OtherEntities").value;
 
-        boolean isMob = ent instanceof MobEntity;
-        boolean isPlayer = ent instanceof PlayerEntity;
-        boolean isItem = ent instanceof ItemEntity;
+        boolean isMob       = ent instanceof MobEntity;
+        boolean isPlayer    = ent instanceof PlayerEntity;
+        boolean isItem      = ent instanceof ItemEntity;
 
         return (
                 (drawPlayer && isPlayer)
@@ -55,6 +66,13 @@ public class Tracers extends Cheat {
             ||  (drawItem && isItem)
             ||  (drawOtherEntities && !isMob && !isPlayer && !isItem)
         );
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean shouldDrawTracer(BlockEntityTickInvoker block) {
+        HashMap<String, Boolean> phrases = (HashMap<String, Boolean>)this.getSetting("SearchPhrases").value;
+
+        return phrases.containsKey(block.getName());
     }
 
     @Override
@@ -83,7 +101,14 @@ public class Tracers extends Cheat {
 
                     // Render tracers
                     RenderUtils.drawTracer(e.mStack, MathsUtils.getLerpedCentre(entity, e.tickDelta), e.tickDelta);
-                    
+                }
+
+                // Render chests etc.
+                List<BlockEntityTickInvoker> tickers = ((IWorld)(CheatClient.getClient().world)).getBlockEntityTickers();
+                for (BlockEntityTickInvoker ticker : tickers) {
+                    if (!this.shouldDrawTracer(ticker)) continue;
+
+                    RenderUtils.drawTracer(e.mStack, BlockUtils.blockPos(ticker.getPos()).add(0.5, 0.5, 0.5), e.tickDelta);
                 }
 
                 break;
