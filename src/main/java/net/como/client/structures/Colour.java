46c9fc1229f48468c85a92cffba8ec6b13b43ac6
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-05 22:10:31 +0100

    Create Colour.java

---
+++ src/main/java/net/como/client/structures/Colour.java
@@ -0,0 +1,12 @@
+package net.como.client.structures;
+
+public class Colour {
+    float r,g,b,a;
+
+    public Colour(float r, float g, float b, float a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+}
