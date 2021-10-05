a14edd4774fde95fdc3e7dab084df45fb1bb614b
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-05 20:34:56 +0100

    Removed pointless comment

--- src/main/java/net/como/client/utils/BlockUtils.java
+++ src/main/java/net/como/client/utils/BlockUtils.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2014-2021 Wurst-Imperium and contributors.
- *
- * This source code is subject to the terms of the GNU General Public
- * License, version 3. If a copy of the GPL was not distributed with this
- * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
- */
-
 // Thanks to the devs at Wurst for this file - it saved me a bit of time :D
 
 package net.como.client.utils;
