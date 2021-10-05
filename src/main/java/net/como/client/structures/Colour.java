36344187c941cdd5e6f72f5093353505482bc40a
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-05 22:14:48 +0100

    Made variables visible

--- src/main/java/net/como/client/structures/Colour.java
+++ src/main/java/net/como/client/structures/Colour.java
@@ -1,7 +1,7 @@
 package net.como.client.structures;
 
 public class Colour {
-    float r,g,b,a;
+    public float r,g,b,a;
 
     public Colour(float r, float g, float b, float a) {
         this.r = r;
