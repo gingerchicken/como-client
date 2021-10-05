b87d14aa0e0d97484b3811540f4a69ded9670c89
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-05 21:19:10 +0100

    Removed unneeded comment

--- src/main/java/net/como/client/utils/RenderUtils.java
+++ src/main/java/net/como/client/utils/RenderUtils.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2014-2021 Wurst-Imperium and contributors.
- *
- * This source code is subject to the terms of the GNU General Public
- * License, version 3. If a copy of the GPL was not distributed with this
- * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
- */
 package net.como.client.utils;
 
 import com.mojang.blaze3d.systems.RenderSystem;
