a1fbe4f6f04e787bce870954d9ffd6e2c5265e0d
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-12 22:05:20 +0100

    Added renderLabelIfPresentEvent

---
+++ src/main/java/net/como/client/events/renderLabelIfPresentEvent.java
@@ -0,0 +1,27 @@
+package net.como.client.events;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.structures.events.Event;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.text.Text;
+
+public class renderLabelIfPresentEvent<T extends Entity> extends Event {
+    public T entity;
+    public Text text;
+	public MatrixStack mStack;
+    public VertexConsumerProvider vertexConsumerProvider;
+	public int i;
+    public CallbackInfo ci;
+
+    public renderLabelIfPresentEvent(T entity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+        this.entity = entity;
+        this.text = text;
+        this.mStack = matrixStack;
+        this.vertexConsumerProvider = vertexConsumerProvider;
+        this.i = i;
+        this.ci = ci;
+    }
+}
