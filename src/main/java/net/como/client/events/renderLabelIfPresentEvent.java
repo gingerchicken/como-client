08c02835d7ebe3da02f1229463a8fd2d0f01002f
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-13 18:57:38 +0100

    Added dispatcher

--- src/main/java/net/como/client/events/renderLabelIfPresentEvent.java
+++ src/main/java/net/como/client/events/renderLabelIfPresentEvent.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.como.client.structures.events.Event;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
@@ -15,13 +16,15 @@ public class renderLabelIfPresentEvent<T extends Entity> extends Event {
     public VertexConsumerProvider vertexConsumerProvider;
 	public int i;
     public CallbackInfo ci;
+    public EntityRenderDispatcher dispatcher;
 
-    public renderLabelIfPresentEvent(T entity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+    public renderLabelIfPresentEvent(T entity, Text text, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, EntityRenderDispatcher dispatcher, CallbackInfo ci) {
         this.entity = entity;
         this.text = text;
         this.mStack = matrixStack;
         this.vertexConsumerProvider = vertexConsumerProvider;
         this.i = i;
+        this.dispatcher = dispatcher;
         this.ci = ci;
     }
 }
