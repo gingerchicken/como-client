eb76639979b32aa1f0e4e8633ebf63d30480b370
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-13 21:02:47 +0100

    Added isInNetherPortal function

---
+++ src/main/java/net/como/client/mixin/botch/NetherMixin.java
@@ -0,0 +1,21 @@
+package net.como.client.mixin.botch;
+
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import net.como.client.interfaces.mixin.IEntity;
+import net.minecraft.entity.Entity;
+
+@Mixin(Entity.class)
+public class NetherMixin implements IEntity {
+    @Shadow
+    protected boolean inNetherPortal;
+
+    @Override
+    public boolean getInNetherPortal() {
+        return this.inNetherPortal;
+    }
+}
