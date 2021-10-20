8f5d99b31cd0ff6e7ca19a08fd5cceb3dd8ba271
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-20 21:52:27 +0100

    Hooked OnSubmersionTypeEvent

---
+++ src/main/java/net/como/client/mixin/CameraMixin.java
@@ -0,0 +1,21 @@
+package net.como.client.mixin;
+
+import javax.security.auth.callback.Callback;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.como.client.CheatClient;
+import net.como.client.events.OnSubmersionTypeEvent;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.CameraSubmersionType;
+
+@Mixin(Camera.class)
+public class CameraMixin {    
+    @Inject(at = @At("RETURN"), method="getSubmersionType()Lnet/minecraft/client/render/CameraSubmersionType;", cancellable = true)
+    public void onGetSubmersionType(CallbackInfoReturnable<CameraSubmersionType> cir) {
+        CheatClient.emitter.triggerEvent(new OnSubmersionTypeEvent(cir));
+    }
+}
