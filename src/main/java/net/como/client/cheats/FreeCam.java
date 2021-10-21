13abd3803581155c39d1ce444b8875d6748b4a46
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-10-21 22:31:35 +0100

    Added revert/set origin functions

--- src/main/java/net/como/client/cheats/FreeCam.java
+++ src/main/java/net/como/client/cheats/FreeCam.java
@@ -7,6 +7,8 @@ import net.como.client.events.SendPacketEvent;
 import net.como.client.structures.Cheat;
 import net.como.client.structures.events.Event;
 import net.como.client.structures.settings.Setting;
+import net.como.client.utils.RotationUtils;
+import net.como.client.utils.RotationUtils.Rotation;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
@@ -14,6 +16,20 @@ import net.minecraft.util.math.Vec3d;
 
 public class FreeCam extends Cheat {
     private Vec3d origin = new Vec3d(0, 0, 0);
+    private float originPitch = 0, originYaw = 0;
+
+    private void setOrigin() {
+        this.origin = CheatClient.me().getPos();
+        this.originYaw = CheatClient.me().getYaw();
+        this.originPitch = CheatClient.me().getPitch();
+        
+    }
+
+    private void revertOrigin() {
+        CheatClient.me().setPos(origin.x, origin.y, origin.z);
+        CheatClient.me().setYaw(this.originYaw);
+        CheatClient.me().setPitch(this.originPitch);
+    }
 
     public FreeCam() {
         super("FreeCam");
@@ -23,20 +39,24 @@ public class FreeCam extends Cheat {
     
     @Override
     public void activate() {
-        origin = CheatClient.me().getPos();
+        this.setOrigin();
 
         this.addListen(ClientTickEvent.class);
         this.addListen(SendPacketEvent.class);
         this.addListen(PlayerMoveEvent.class);
+
+        this.revertOrigin();
     }
 
     @Override
     public void deactivate() {
-        CheatClient.me().setPos(origin.x, origin.y, origin.z);
+        this.revertOrigin();
 
         this.removeListen(ClientTickEvent.class);
         this.removeListen(SendPacketEvent.class);
         this.removeListen(PlayerMoveEvent.class);
+
+        this.revertOrigin();
     }
 
     @Override
