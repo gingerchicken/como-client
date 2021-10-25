7e9a2b3fe58ce8450697c5490d9862e55eb59a16
gingerchicken <32576907+gingerchicken@users.noreply.github.com>
2021-10-25 13:55:42 +0100

    Added getFontManager function

--- src/main/java/net/como/client/mixin/client/MinecraftClientMixin.java
+++ src/main/java/net/como/client/mixin/client/MinecraftClientMixin.java
@@ -1,16 +1,20 @@
 package net.como.client.mixin.client;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.como.client.CheatClient;
 import net.como.client.events.OnClientCloseEvent;
+import net.como.client.interfaces.mixin.IClient;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.FontManager;
 
 @Mixin(MinecraftClient.class)
-public class MinecraftClientMixin {
+public class MinecraftClientMixin implements IClient {
     @Inject(at = @At("HEAD"), method = "close()V", cancellable = false)
     private void onClose(CallbackInfo ci) {
         CheatClient.emitter.triggerEvent(new OnClientCloseEvent(ci));
@@ -18,4 +22,15 @@ public class MinecraftClientMixin {
         // Close our client after all is said and done.
         CheatClient.close();
     }
+
+    @Shadow
+    @Final
+    FontManager fontManager;
+
+    @Override
+    public FontManager getFontManager() {
+        return fontManager;
+    }
+
+    
 }
