b0b6d0a822fe02fa9e85457c027d2958ebde4f52
gingerchicken <32576907+gingerchicken@users.noreply.github.com>
2021-10-25 13:58:51 +0100

    Implemented CheatClient textRenderer

--- src/main/java/net/como/client/cheats/BetterNameTags.java
+++ src/main/java/net/como/client/cheats/BetterNameTags.java
@@ -140,7 +140,7 @@ public class BetterNameTags extends Cheat {
 
     private void displayNameTag(PlayerEntity player, MatrixStack mStack, float tickDelta) {
         Vec3d pos = player.getLerpedPos(tickDelta).add(0, player.getBoundingBox().maxY - player.getPos().y + 0.25, 0);
-        TextRenderer r = CheatClient.getClient().textRenderer;
+        TextRenderer r = CheatClient.textRenderer;
 
         float textOffsets = 2.5f;
         Attribute[] attributes = {
