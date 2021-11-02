48a6193629bc95a1d84239e7701ac8832817a587
gingerchicken <32576907+gingerchicken@users.noreply.github.com>
2021-11-02 19:53:16 +0000

    Created getEntFlag function
    This is a public version of the getFlag function

--- src/main/java/net/como/client/mixin/botch/EntityMixin.java
+++ src/main/java/net/como/client/mixin/botch/EntityMixin.java
@@ -1,21 +1,34 @@
 package net.como.client.mixin.botch;
 
-import java.util.UUID;
-import java.util.stream.Stream;
-
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 import net.como.client.interfaces.mixin.IEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
 
 @Mixin(Entity.class)
 public class EntityMixin implements IEntity {
     @Shadow
     protected boolean inNetherPortal;
 
+    @Shadow
+    @Final
+    protected static TrackedData<Byte> FLAGS;
+
+    @Shadow
+    @Final
+    protected DataTracker dataTracker;
+
     @Override
     public boolean getInNetherPortal() {
         return this.inNetherPortal;
+    }
+
+    @Override
+    public boolean getEntFlag(int index) {
+        return (this.dataTracker.get(FLAGS) & 1 << index) != 0;
     }
 }
