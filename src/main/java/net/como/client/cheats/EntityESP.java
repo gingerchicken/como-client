a2ec7c4b991349f799db5f9aff17c83eb38a7a0a
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-11-03 23:15:13 +0000

    Added colouring for the entity glow

--- src/main/java/net/como/client/cheats/EntityESP.java
+++ src/main/java/net/como/client/cheats/EntityESP.java
@@ -3,11 +3,14 @@ package net.como.client.cheats;
 import net.como.client.CheatClient;
 import net.como.client.events.IsEntityGlowingEvent;
 import net.como.client.events.OnRenderEvent;
-import net.como.client.events.RenderWorldViewBobbingEvent;
+import net.como.client.events.RenderEntityEvent;
 import net.como.client.structures.Cheat;
+import net.como.client.structures.Colour;
 import net.como.client.structures.events.Event;
 import net.como.client.structures.settings.Setting;
 import net.como.client.utils.RenderUtils;
+import net.minecraft.client.render.OutlineVertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 
@@ -19,9 +22,14 @@ public class EntityESP extends Cheat {
     public EntityESP() {
         super("EntityESP");
 
+        // Bounding Boxes
         this.addSetting(new Setting("BoxPadding", 0f));
         this.addSetting(new Setting("BlendBoxes", false));
 
+        // Glow
+        this.addSetting(new Setting("GlowColour", false));
+
+        // Drawing Mode setting
         this.addSetting(new Setting("DrawMode", MODE_GLOW));
 
         this.description = "Know where entities are more easily.";
@@ -31,32 +39,73 @@ public class EntityESP extends Cheat {
     public void activate() {
         this.addListen(OnRenderEvent.class);
         this.addListen(IsEntityGlowingEvent.class);
+        this.addListen(RenderEntityEvent.class);
     }
 
     @Override
 	public void deactivate() {
         this.removeListen(OnRenderEvent.class);
         this.removeListen(IsEntityGlowingEvent.class);
+        this.removeListen(RenderEntityEvent.class);
 	}
 
     private boolean shouldRender(Entity entity) {
         return !(entity instanceof PlayerEntity && (PlayerEntity)entity == CheatClient.me());
     }
 
+    // This is just for normalising the setting till we get a enum setting type.
+    // TODO enum setting type when?
     private int getDrawMode() {
-        return (int)this.getSetting("DrawMode").value;
+        int mode = (int)this.getSetting("DrawMode").value;
+
+        switch (mode) {
+            case MODE_GLOW:
+            case MODE_MOB_BOX: {
+                return mode;
+            }
+            default: return 1;
+        }
+    }
+
+    // Just a wrapper for rendering the boxes
+    private void renderBoxes(Entity entity, float tickDelta, MatrixStack mStack) {
+        RenderUtils.renderBox(entity, tickDelta, mStack, (Boolean)this.getSetting("BlendBoxes").value, (Float)this.getSetting("BoxPadding").value);
     }
 
     @Override
     public void fireEvent(Event event) {
         switch (event.getClass().getSimpleName()) {
+            // For entity glow
             case "IsEntityGlowingEvent": {
                 if (this.getDrawMode() != MODE_GLOW) break;
 
                 IsEntityGlowingEvent e = (IsEntityGlowingEvent)event;
+                if (!this.shouldRender(e.entity)) break;
 
-                if (!shouldRender(e.entity)) break;
                 e.cir.setReturnValue(true);
+
+                break;
+            }
+            case "RenderEntityEvent": {
+                if (this.getDrawMode() != MODE_GLOW) break;
+
+                RenderEntityEvent e = (RenderEntityEvent)event;
+
+                // Don't bother if we don't want them.
+                if (!this.shouldRender(e.entity)) break;
+
+                // Make sure we have the right vertexConsumers
+                if (!(e.vertexConsumers instanceof OutlineVertexConsumerProvider)) {
+                    break;
+                }
+
+                OutlineVertexConsumerProvider outlineVertexConsumers = (OutlineVertexConsumerProvider)(e.vertexConsumers);
+
+                // Calculate what colour we want
+                Colour colour = ((Boolean)this.getSetting("GlowColour").value) ? Colour.fromDistance(e.entity) : new Colour(255, 255, 255, 255);
+
+                // Set the colour
+                outlineVertexConsumers.setColor((int)colour.r, (int)colour.g, (int)colour.b, (int)colour.a);
 
                 break;
             }
@@ -67,15 +116,13 @@ public class EntityESP extends Cheat {
                 Iterable<Entity> ents = CheatClient.getClient().world.getEntities();
 
                 for (Entity entity : ents) {
-                    // No render myself.
+                    // Don't render stuff we don't want to see!
                     if (!this.shouldRender(entity)) {
                         continue;
                     }
 
-                    // Render mob box
-                    RenderUtils.renderBox(entity, e.tickDelta, e.mStack, (Boolean)this.getSetting("BlendBoxes").value, (Float)this.getSetting("BoxPadding").value);
+                    this.renderBoxes(entity, e.tickDelta, e.mStack);
                 }
-
                 break;
             }
         }
