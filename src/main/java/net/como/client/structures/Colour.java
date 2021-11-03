8f0a5ab057ab6ba257af2274f316c49b7f6174f7
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-11-03 23:06:22 +0000

    Clamped inputs

--- src/main/java/net/como/client/structures/Colour.java
+++ src/main/java/net/como/client/structures/Colour.java
@@ -3,10 +3,20 @@ package net.como.client.structures;
 public class Colour {
     public float r,g,b,a;
 
+    private static float clamp(float x) {
+        if (x > 255) return 255.0f;
+        if (x < 0) return 0f;
+
+        return x;
+    }
+
     public Colour(float r, float g, float b, float a) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
+
+        this.r = clamp(r);
+        this.g = clamp(g);
+        this.b = clamp(b);
+        this.a = clamp(a);
+    }
+
     }
 }
