8027b3c8e24cb528633a95dca371ac96cdf85cd6
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-11-03 22:18:52 +0000

    Added basic glowing effect

--- src/main/java/net/como/client/cheats/EntityESP.java
+++ src/main/java/net/como/client/cheats/EntityESP.java
@@ -1,6 +1,7 @@
 package net.como.client.cheats;
 
 import net.como.client.CheatClient;
+import net.como.client.events.IsEntityGlowingEvent;
 import net.como.client.events.OnRenderEvent;
 import net.como.client.events.RenderWorldViewBobbingEvent;
 import net.como.client.structures.Cheat;
@@ -12,12 +13,16 @@ import net.minecraft.entity.player.PlayerEntity;
 
 
 public class EntityESP extends Cheat {
+    private final static int MODE_GLOW      = 1;
+    private final static int MODE_MOB_BOX   = 2;
+
     public EntityESP() {
         super("EntityESP");
 
         this.addSetting(new Setting("BoxPadding", 0f));
         this.addSetting(new Setting("BlendBoxes", false));
-        this.addSetting(new Setting("BoundingBox", true)); // TODO make this a mode
+
+        this.addSetting(new Setting("DrawMode", MODE_GLOW));
 
         this.description = "Know where entities are more easily.";
     }
@@ -25,30 +30,50 @@ public class EntityESP extends Cheat {
     @Override
     public void activate() {
         this.addListen(OnRenderEvent.class);
+        this.addListen(IsEntityGlowingEvent.class);
     }
 
     @Override
 	public void deactivate() {
         this.removeListen(OnRenderEvent.class);
+        this.removeListen(IsEntityGlowingEvent.class);
 	}
+
+    private boolean shouldRender(Entity entity) {
+        return !(entity instanceof PlayerEntity && (PlayerEntity)entity == CheatClient.me());
+    }
+
+    private int getDrawMode() {
+        return (int)this.getSetting("DrawMode").value;
+    }
 
     @Override
     public void fireEvent(Event event) {
         switch (event.getClass().getSimpleName()) {
+            case "IsEntityGlowingEvent": {
+                if (this.getDrawMode() != MODE_GLOW) break;
+
+                IsEntityGlowingEvent e = (IsEntityGlowingEvent)event;
+
+                if (!shouldRender(e.entity)) break;
+                e.cir.setReturnValue(true);
+
+                break;
+            }
             case "OnRenderEvent": {
+                if (this.getDrawMode() != MODE_MOB_BOX) break;
+
                 OnRenderEvent e = (OnRenderEvent)event;
                 Iterable<Entity> ents = CheatClient.getClient().world.getEntities();
 
                 for (Entity entity : ents) {
                     // No render myself.
-                    if (entity instanceof PlayerEntity && (PlayerEntity)entity == CheatClient.me()) {
+                    if (!this.shouldRender(entity)) {
                         continue;
                     }
 
                     // Render mob box
-                    if ((Boolean)this.getSetting("BoundingBox").value) {
-                        RenderUtils.renderBox(entity, e.tickDelta, e.mStack, (Boolean)this.getSetting("BlendBoxes").value, (Float)this.getSetting("BoxPadding").value);
-                    }
+                    RenderUtils.renderBox(entity, e.tickDelta, e.mStack, (Boolean)this.getSetting("BlendBoxes").value, (Float)this.getSetting("BoxPadding").value);
                 }
 
                 break;
