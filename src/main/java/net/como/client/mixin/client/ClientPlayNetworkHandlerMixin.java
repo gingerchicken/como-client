9d940d8a2421e6ff660789a1dec828ff82bd90c4
gingerchicken <32576907+gingerchicken@users.noreply.github.com>
2021-11-07 15:23:55 +0000

    Hooked OnWorldTimeUpdateEvent

--- src/main/java/net/como/client/mixin/client/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/net/como/client/mixin/client/ClientPlayNetworkHandlerMixin.java
@@ -6,14 +6,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.At;
 
 import net.como.client.CheatClient;
+import net.como.client.events.OnWorldTimeUpdateEvent;
 import net.como.client.events.SendPacketEvent;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.Packet;
+import net.minecraft.network.packet.s2c.play.WorldTimeUpdateS2CPacket;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
     @Inject(at = @At("HEAD"), method="sendPacket(Lnet/minecraft/network/Packet;)V", cancellable = true)
     public void onSendPacket(Packet<?> packet, CallbackInfo ci) {
         CheatClient.emitter.triggerEvent(new SendPacketEvent(packet, ci));
+    }
+
+    @Inject(at = @At("HEAD"), method="onWorldTimeUpdate(Lnet/minecraft/network/packet/s2c/play/WorldTimeUpdateS2CPacket;)V", cancellable=true)
+    public void onWorldTimeUpdate(WorldTimeUpdateS2CPacket packet, CallbackInfo ci) {
+        CheatClient.emitter.triggerEvent(new OnWorldTimeUpdateEvent(packet, ci));
     }
 }
