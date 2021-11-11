9537a1387ebbc40e6cc7a1c7a2e322efba4d4390
gingerchicken <32576907+gingerchicken@users.noreply.github.com>
2021-11-11 23:31:09 +0000

    Commented the code

--- src/main/java/net/como/client/cheats/ArmourDisplay.java
+++ src/main/java/net/como/client/cheats/ArmourDisplay.java
@@ -17,6 +17,7 @@ import net.como.client.structures.events.Event;
 import net.como.client.structures.settings.Setting;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.util.Window;
 import net.minecraft.item.ItemStack;
 import net.minecraft.client.MinecraftClient;
 
@@ -45,30 +46,40 @@ public class ArmourDisplay extends Cheat {
 
         Boolean renderEmpty = (Boolean)this.getSetting("RenderEmpty").value;
 
+        // The width of the item on screen
         int width = 18;
 
+        // Get the armour
         List<ItemStack> armour = new ArrayList<ItemStack>();
 
+        // Iterate backwards over the armour set
         for (int i = 3; i >= 0; i--) {
+            // Get the piece
             ItemStack piece = CheatClient.me().getInventory().getArmorStack(i);
 
+            // If the piece is nothing and we don't want to render nothing, don't add it to the list to be rendered.
             if (!piece.isEmpty() || renderEmpty) {
                 armour.add(piece);
             }
         }
 
+        // Get the total size
         int totalWidth = armour.size()*width;
 
+        // Calculate the padding to "centre" the items.
         int padLeft = (length - totalWidth) / 2;
 
+        // Get the position where we are going to render the items.
         int curX  = x + padLeft;
         int curY  = y;
 
         // Render each item
         for (ItemStack item : armour) {
+            // Render the item on the screen
             ir.renderInGuiWithOverrides(item, curX, curY);
             ir.renderGuiItemOverlay(r, item, curX, curY);
 
+            // Calculate where we are going to render the next item
             curX += width;
         }
     }
@@ -78,8 +89,13 @@ public class ArmourDisplay extends Cheat {
         switch (event.getClass().getSimpleName()) {
             case "InGameHudRenderEvent": {
                 InGameHudRenderEvent e = (InGameHudRenderEvent)event;
-                
-                this.renderDisplay(CheatClient.getClient().getWindow().getScaledWidth()/2 + 5, CheatClient.getClient().getWindow().getScaledHeight() - 55, 91);
+                Window window = CheatClient.getClient().getWindow();
+
+                this.renderDisplay(
+                    window.getScaledWidth() / 2 + 5,
+                    window.getScaledHeight() - 55,
+                    91
+                );
                 
 
                 break;
