66e6edaea5cc90a2470d76bddb43fab0c8016633
gingerchicken <32576907+gingerchicken@users.noreply.github.com>
2021-11-13 17:08:32 +0000

    Added rightClickBlock function

--- src/main/java/net/como/client/utils/InteractionUtils.java
+++ src/main/java/net/como/client/utils/InteractionUtils.java
@@ -1,8 +1,15 @@
 package net.como.client.utils;
 
 import net.como.client.CheatClient;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 
 public class InteractionUtils {
     public static ClientPlayerInteractionManager getManager() {        
@@ -11,5 +18,17 @@ public class InteractionUtils {
 
     public static void pickupItem(int slot) {
         getManager().clickSlot(0, slot, 0, SlotActionType.PICKUP, CheatClient.me());
+    }
+
+    public static void rightClickBlock(BlockPos pos, Direction side, Vec3d hitVec) {
+        ClientPlayerInteractionManager im = getManager();
+        MinecraftClient client = CheatClient.getClient();
+        ClientPlayerEntity me = CheatClient.me();
+
+        // Send block packet
+        im.interactBlock(me, client.world, Hand.MAIN_HAND, new BlockHitResult(hitVec, side, pos, false));
+
+        // Send item packet
+		im.interactItem(client.player, client.world, Hand.MAIN_HAND);
     }
 }
