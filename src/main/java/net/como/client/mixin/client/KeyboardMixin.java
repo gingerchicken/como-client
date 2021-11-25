f03600003fcff3cbceb1d95cf3585c0a2a486816
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-11-25 14:33:10 +0000

    Hooked OnKeyEvent

---
+++ src/main/java/net/como/client/mixin/client/KeyboardMixin.java
@@ -0,0 +1,18 @@
+package net.como.client.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.como.client.CheatClient;
+import net.como.client.events.OnKeyEvent;
+import net.minecraft.client.Keyboard;
+
+@Mixin(Keyboard.class)
+public class KeyboardMixin {
+    @Inject(at = @At("HEAD"), method="onKey(JIIII)V", cancellable = true)
+    private void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        CheatClient.emitter.triggerEvent(new OnKeyEvent(window, key, scancode, action, modifiers, ci));
+    }
+}
