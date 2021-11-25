f8323487275952a872e5569b75864b132a79908a
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-11-25 14:31:13 +0000

    Create OnKeyEvent

---
+++ src/main/java/net/como/client/events/OnKeyEvent.java
@@ -0,0 +1,23 @@
+package net.como.client.events;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.structures.events.Event;
+
+public class OnKeyEvent extends Event {
+    public long window;
+    public int key;
+    public int scancode;
+    public int action;
+    public int modifiers;
+    public CallbackInfo ci;
+
+    public OnKeyEvent(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        this.window = window;
+        this.key = key;
+        this.scancode = scancode;
+        this.action = action;
+        this.modifiers = modifiers;
+        this.ci = ci;
+    }
+}
