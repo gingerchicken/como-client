0f40a82e4bba066cb5f8b2c8119bd46784b25e91
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-11-28 20:31:44 +0000

    Hooked OnEntityStatusEvent

--- src/main/java/net/como/client/mixin/client/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/net/como/client/mixin/client/ClientPlayNetworkHandlerMixin.java
@@ -6,10 +6,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.At;
 
 import net.como.client.CheatClient;
+import net.como.client.events.OnEntityStatusEvent;
 import net.como.client.events.OnWorldTimeUpdateEvent;
 import net.como.client.events.SendPacketEvent;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.Packet;
+import net.minecraft.network.packet.s2c.play.EntityStatusS2CPacket;
 import net.minecraft.network.packet.s2c.play.WorldTimeUpdateS2CPacket;
 
 @Mixin(ClientPlayNetworkHandler.class)
@@ -22,5 +24,10 @@ public class ClientPlayNetworkHandlerMixin {
     @Inject(at = @At("HEAD"), method="onWorldTimeUpdate(Lnet/minecraft/network/packet/s2c/play/WorldTimeUpdateS2CPacket;)V", cancellable=true)
     public void onWorldTimeUpdate(WorldTimeUpdateS2CPacket packet, CallbackInfo ci) {
         CheatClient.emitter.triggerEvent(new OnWorldTimeUpdateEvent(packet, ci));
+    }
+
+    @Inject(at = @At("HEAD"), method="onEntityStatus(Lnet/minecraft/network/packet/s2c/play/EntityStatusS2CPacket;)V", cancellable = true)
+    public void onEntityStatus(EntityStatusS2CPacket packet, CallbackInfo ci) {
+        CheatClient.emitter.triggerEvent(new OnEntityStatusEvent(packet, ci));
     }
 }
