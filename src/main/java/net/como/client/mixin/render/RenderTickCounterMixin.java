88549dbce5f232d22ef1424494acf3723c5c71da
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-12-09 16:26:07 +0000

    Added BeginRenderTickEvent

---
+++ src/main/java/net/como/client/mixin/render/RenderTickCounterMixin.java
@@ -0,0 +1,20 @@
+package net.como.client.mixin.render;
+
+import javax.security.auth.callback.Callback;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.como.client.ComoClient;
+import net.como.client.events.BeginRenderTickEvent;
+import net.minecraft.client.render.RenderTickCounter;
+
+@Mixin(RenderTickCounter.class)
+public class RenderTickCounterMixin {
+    @Inject(at = @At("HEAD"), method = "beginRenderTick(J)I", cancellable = true)
+    private void onBeginRenderTick(long timeMillis, CallbackInfoReturnable<Integer> ci) {
+        ComoClient.emitter.triggerEvent(new BeginRenderTickEvent(timeMillis, ci));
+    }
+}
