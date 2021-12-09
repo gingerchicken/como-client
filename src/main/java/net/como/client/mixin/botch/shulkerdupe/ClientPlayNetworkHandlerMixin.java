63fba5ca654482e146966348b37a5e11dba25754
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-12-09 00:23:55 +0000

    Resolved #128

---
+++ src/main/java/net/como/client/mixin/botch/shulkerdupe/ClientPlayNetworkHandlerMixin.java
@@ -0,0 +1,24 @@
+package net.como.client.mixin.botch.shulkerdupe;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.como.client.ComoClient;
+import net.como.client.modules.ShulkerDupe;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.Packet;
+
+
+@Mixin(ClientPlayNetworkHandler.class)
+public class ClientPlayNetworkHandlerMixin {
+
+    // The shulker dupe needs to be tick perfect hence we are directly hooking it.
+    @Inject(at = @At("TAIL"), method="sendPacket(Lnet/minecraft/network/Packet;)V", cancellable = true)
+    public void onSendPacket(Packet<?> packet, CallbackInfo ci) {
+        ShulkerDupe shulkerDupe = (ShulkerDupe)(ComoClient.Cheats.get("shulkerdupe"));
+
+        if (shulkerDupe.isEnabled()) shulkerDupe.handlePacket(packet);
+    }
+}
