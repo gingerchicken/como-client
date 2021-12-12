9d625bc28d93f84b2c8baed77def46c3fdbbe85e
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-12-12 13:52:34 +0000

    Resolved #134
    Added Potion Effect option

--- src/main/java/net/como/client/modules/FullBright.java
+++ src/main/java/net/como/client/modules/FullBright.java
@@ -1,30 +1,80 @@
 package net.como.client.modules;
 
 import net.como.client.ComoClient;
+import net.como.client.events.ClientTickEvent;
 import net.como.client.structures.Module;
+import net.como.client.structures.events.Event;
+import net.como.client.structures.settings.Setting;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 
 public class FullBright extends Module {
     private Double normalGamma = 0d;
+    Boolean hasSetGamma = false;
 
     public FullBright() {
         super("FullBright");
         
         this.description = "Allows you to see anywhere as if it was day.";
+
+        // TODO change this once you add EnumSettings
+        this.addSetting(new Setting("PotionEffect", false));
     }
     
     @Override
     public void activate() {
-        MinecraftClient client = ComoClient.getClient();
+        this.addListen(ClientTickEvent.class);
+    }
 
-        this.normalGamma = client.options.gamma;
-        client.options.gamma = 16;
+    private void restoreGamma() {
+        if (!this.hasSetGamma) return;
+
+        MinecraftClient client = ComoClient.getClient();
+        client.options.gamma = this.normalGamma;
+        this.hasSetGamma = false;
+    }
+
+    private void restoreEffect() {
+        if (ComoClient.me().hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+            ComoClient.me().removeStatusEffect(StatusEffects.NIGHT_VISION);
+        }
     }
 
     @Override
     public void deactivate() {
-        MinecraftClient client = ComoClient.getClient();
+        this.removeListen(ClientTickEvent.class);
 
-        client.options.gamma = this.normalGamma;
+        this.restoreGamma();
+        this.restoreEffect();
+    }
+
+    @Override
+    public void fireEvent(Event event) {
+        switch (event.getClass().getSimpleName()) {
+            case "ClientTickEvent": {
+                if (this.getBoolSetting("PotionEffect")) {
+                    // Restore other mod's gamma
+                    this.restoreGamma();
+
+                    ComoClient.me().addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 3, 1, true, true));
+
+                    break;
+                }
+
+                MinecraftClient client = ComoClient.getClient();
+                if (!this.hasSetGamma) {
+                    this.normalGamma = client.options.gamma;
+                    this.hasSetGamma = true;
+
+                    this.restoreEffect();
+                }
+
+                client.options.gamma = 16d;
+                break;
+            }
+        }
     }
 }
