48d41fd0ff11d095c6f882b316afc03d6cae3e46
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-12-12 14:08:12 +0000

    Resolved #132

--- src/main/java/net/como/client/Main.java
+++ src/main/java/net/como/client/Main.java
@@ -1,11 +1,14 @@
 package net.como.client;
 
+// Oh no
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.fabricmc.api.ModInitializer;
 
 public class Main implements ModInitializer {
-
     @Override
     public void onInitialize() {
-        System.out.println("Remember, it's only blockgame :V");
+        ComoClient.log("Remember, it's only blockgame :V");
     }
 }
