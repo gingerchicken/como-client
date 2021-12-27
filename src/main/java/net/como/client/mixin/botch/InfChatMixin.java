1b49a6309d61d612d6d2c4ffb536eed7296f4f22
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-12-27 17:56:34 +0000

    Hooked infchat
    Resolved #142

---
+++ src/main/java/net/como/client/mixin/botch/InfChatMixin.java
@@ -0,0 +1,27 @@
+package net.como.client.mixin.botch;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.ComoClient;
+import net.como.client.modules.InfChat;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+
+@Mixin(ChatScreen.class)
+public abstract class InfChatMixin {
+    @Shadow
+    protected TextFieldWidget chatField;
+
+    @Inject(at = @At("TAIL"), method="init()V")
+    private void onInit(CallbackInfo ci) {
+        InfChat infChat = (InfChat)(ComoClient.Modules.get("infchat"));
+        if (!infChat.isEnabled()) return;
+
+        chatField.setMaxLength(Integer.MAX_VALUE);
+    }
+
+}
