a8d909a8a8b7b835cb52a7224d831a78a2ca78e0
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-12-27 21:43:32 +0000

    Removed unused imports

--- src/main/java/net/como/client/gui/SimpleContainer.java
+++ src/main/java/net/como/client/gui/SimpleContainer.java
@@ -1,21 +1,8 @@
 package net.como.client.gui;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-
-import org.lwjgl.opengl.GL11;
-
-import net.como.client.ComoClient;
 import net.como.client.utils.Render2DUtils;
-import net.como.client.utils.RenderUtils;
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.BufferRenderer;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.math.Vec2f;
-import net.minecraft.client.render.VertexFormats;
-import net.minecraft.client.render.VertexFormat;
 
 public class SimpleContainer extends Widget {
 
