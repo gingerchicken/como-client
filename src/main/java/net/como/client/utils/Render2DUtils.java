30c8b9bca480713f117eaf947ad8ad5c595d6269
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-12-28 02:24:07 +0000

    Added renderBackground
    This will render a quad with a given coloured background

--- src/main/java/net/como/client/utils/Render2DUtils.java
+++ src/main/java/net/como/client/utils/Render2DUtils.java
@@ -12,8 +12,10 @@ import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.util.Window;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Matrix4f;
+import net.minecraft.util.math.Vec2f;
 
 public class Render2DUtils {
     public static class BufferContainer {
@@ -44,6 +46,17 @@ public class Render2DUtils {
         public void vertex2D(int x, int y) {
             this.bufferBuilder.vertex(x, y, 0).next();
         }
+    }
+
+    public static Vec2f relPosition(Vec2f vec) {
+        return relPosition((int)vec.x, (int)vec.y);
+    }
+
+    public static Vec2f relPosition(int x, int y) {
+        return new Vec2f(
+            (float)x,
+            (float)y
+        );
     }
     
     public static void begin(MatrixStack matrixStack, Colour colour) {
@@ -111,5 +124,23 @@ public class Render2DUtils {
 
     public static void renderBox(MatrixStack matrixStack, int x, int y, int x2, int y2) {
         renderBox(matrixStack, x, y, x2, y2, new Colour(255, 255, 255, 255));
+    }
+
+    public static void renderBackground(MatrixStack matrixStack, int x, int y, int x2, int y2, Colour colour) {
+        begin(matrixStack, colour);
+        BufferContainer bufferContainer = new BufferContainer(matrixStack, VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
+
+        bufferContainer.vertex2D(x, y);
+        bufferContainer.vertex2D(x, y2);
+        bufferContainer.vertex2D(x2, y2);
+        bufferContainer.vertex2D(x2, y);
+
+        bufferContainer.close();
+        finish(matrixStack);
+    }
+
+    public static void renderBackgroundBox(MatrixStack matrixStack, int x, int y, int x2, int y2, Colour bgColour, Colour outline) {
+        renderBackground(matrixStack, x, y, x2, y2, bgColour);
+        renderBox(matrixStack, x, y, x2, y2, outline);
     }
 }
