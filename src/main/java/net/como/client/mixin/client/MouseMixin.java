a5c64323095f720d3d5a77006c6cbb303eea8f88
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-01-02 02:34:27 +0000

    Hooked OnMouseButton

---
+++ src/main/java/net/como/client/mixin/client/MouseMixin.java
@@ -0,0 +1,18 @@
+package net.como.client.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.ComoClient;
+import net.como.client.events.OnMouseButtonEvent;
+import net.minecraft.client.Mouse;
+
+@Mixin(Mouse.class)
+public class MouseMixin {
+    @Inject(at = @At("HEAD"), method="onMouseButton(JIII)V", cancellable = true)
+    private void onMouseButton(long window, int button, int action, int mods, CallbackInfo ci) {
+        ComoClient.emitter.triggerEvent(new OnMouseButtonEvent(window, button, action, mods, ci));
+    }
+}
