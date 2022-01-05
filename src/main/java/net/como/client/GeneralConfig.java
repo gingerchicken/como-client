25da1dc28bcf10d09dfcc0240d227df37f85edd1
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-01-05 16:11:16 +0000

    Changed default command prefix
    Helps with other client compatablility

--- src/main/java/net/como/client/GeneralConfig.java
+++ src/main/java/net/como/client/GeneralConfig.java
@@ -6,7 +6,7 @@ public class GeneralConfig {
     public Colour storageColour = new Colour(255, 223, 0, 255);
     public Colour entityColour  = new Colour(255, 255, 255, 255);
     public String font = "como-client:como";
-    public String commandPrefix = ".";
+    public String commandPrefix = ",";
 
     public GeneralConfig() {
 
