d090067fd264db32153b5d13546707fec3e5c08f
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-01-09 23:00:52 +0000

    Added StopUsingItemEvent

---
+++ src/main/java/net/como/client/events/StopUsingItemEvent.java
@@ -0,0 +1,16 @@
+package net.como.client.events;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.structures.events.Event;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class StopUsingItemEvent extends Event {
+    public PlayerEntity playerEntity;
+    public CallbackInfo ci;
+    
+    public StopUsingItemEvent(PlayerEntity playerEntity, CallbackInfo ci) {
+        this.playerEntity = playerEntity;
+        this.ci = ci;
+    }
+}
