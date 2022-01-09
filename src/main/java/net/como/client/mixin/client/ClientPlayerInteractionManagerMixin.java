12b4c587c489873c6935f583769fdaf3286a83a8
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-01-09 23:01:06 +0000

    Hooked StopUsingItemEvent

--- src/main/java/net/como/client/mixin/client/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/net/como/client/mixin/client/ClientPlayerInteractionManagerMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.como.client.ComoClient;
 import net.como.client.events.OnAttackEntityEvent;
+import net.como.client.events.StopUsingItemEvent;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,5 +17,10 @@ public class ClientPlayerInteractionManagerMixin {
     @Inject(at = @At("HEAD"), method = "attackEntity(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/entity/Entity;)V", cancellable = true)
     private void onAttackEntity(PlayerEntity player, Entity target, CallbackInfo ci) {
         ComoClient.emitter.triggerEvent(new OnAttackEntityEvent(player, target, ci));
-    }    
+    }
+
+    @Inject(at = @At("HEAD"), method = "stopUsingItem(Lnet/minecraft/entity/player/PlayerEntity;)V", cancellable = true)
+    private void onStopUsingItem(PlayerEntity playerEntity, CallbackInfo ci) {
+        ComoClient.emitter.triggerEvent(new StopUsingItemEvent(playerEntity, ci));
+    }
 }
