f47bfc6c04862071833b79bbda4c243fddb2cec0
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-01-13 00:32:22 +0000

    Added SignRenderEvent

---
+++ src/main/java/net/como/client/events/SignRenderEvent.java
@@ -0,0 +1,28 @@
+package net.como.client.events;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.structures.events.Event;
+import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+
+public class SignRenderEvent extends Event {
+    public SignBlockEntity signBlockEntity;
+    public float f;
+    public MatrixStack matrixStack;
+    public VertexConsumerProvider vertexConsumerProvider;
+    public int light;
+    public int overlay;
+    public CallbackInfo ci;
+
+    public SignRenderEvent(SignBlockEntity signBlockEntity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light, int overlay, CallbackInfo ci) {
+        this.signBlockEntity = signBlockEntity;
+        this.f = f;
+        this.matrixStack = matrixStack;
+        this.vertexConsumerProvider = vertexConsumerProvider;
+        this.light = light;
+        this.overlay = overlay;
+        this.ci = ci;
+    }
+}
