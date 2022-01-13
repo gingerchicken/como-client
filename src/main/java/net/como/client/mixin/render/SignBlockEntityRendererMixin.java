f7bd4690a171ac66d37234c7c1a93d86d0ffe803
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-01-13 00:32:41 +0000

    Hooked SignRenderEvent

---
+++ src/main/java/net/como/client/mixin/render/SignBlockEntityRendererMixin.java
@@ -0,0 +1,21 @@
+package net.como.client.mixin.render;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.como.client.ComoClient;
+import net.como.client.events.SignRenderEvent;
+import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.SignBlockEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+
+@Mixin(SignBlockEntityRenderer.class)
+public class SignBlockEntityRendererMixin {
+    @Inject(at = @At("HEAD"), method="render(Lnet/minecraft/block/entity/SignBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V")
+    private void onRender(SignBlockEntity signBlockEntity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light, int overlay, CallbackInfo ci) {
+        ComoClient.emitter.triggerEvent(new SignRenderEvent(signBlockEntity, f, matrixStack, vertexConsumerProvider, light, overlay, ci));
+    }
+}
