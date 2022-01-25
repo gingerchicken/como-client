e76856bcf8dab9b5940ee83a0bfb28bbde46b080
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-01-25 00:12:39 +0000

    Added packet blocking
    it will block any packets saying that we are using fabric

--- src/main/java/net/como/client/modules/packet/FakeClient.java
+++ src/main/java/net/como/client/modules/packet/FakeClient.java
@@ -1,9 +1,19 @@
 package net.como.client.modules.packet;
 
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.lang3.StringUtils;
+
+import io.netty.buffer.Unpooled;
 import net.como.client.events.GetClientModNameEvent;
+import net.como.client.events.SendPacketEvent;
+import net.como.client.mixin.packet.CustomPayloadC2SPacketAccessor;
 import net.como.client.structures.Module;
 import net.como.client.structures.events.Event;
 import net.como.client.structures.settings.Setting;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
+import net.minecraft.util.Identifier;
 
 public class FakeClient extends Module {
     public FakeClient() {
@@ -22,12 +32,18 @@ public class FakeClient extends Module {
 
     @Override
     public void activate() {
+        this.addListen(SendPacketEvent.class);
         this.addListen(GetClientModNameEvent.class);
     }
 
     @Override
     public void deactivate() {
+        this.removeListen(SendPacketEvent.class);
         this.removeListen(GetClientModNameEvent.class);
+    }
+
+    private String getClientName() {
+        return this.getStringSetting("Client");
     }
 
     @Override
@@ -36,7 +52,38 @@ public class FakeClient extends Module {
             case "GetClientModNameEvent": {
                 GetClientModNameEvent e = (GetClientModNameEvent)event;
 
-                e.cir.setReturnValue(this.getStringSetting("Client"));
+                e.cir.setReturnValue(this.getClientName());
+
+                break;
+            }
+
+            case "SendPacketEvent": {
+                SendPacketEvent e = (SendPacketEvent)event;
+
+                // Shout out to the meteor developers for this one!
+
+                if (!(e.packet instanceof CustomPayloadC2SPacket)) break;
+                CustomPayloadC2SPacketAccessor packet = (CustomPayloadC2SPacketAccessor)(e.packet);
+                
+                Identifier id = packet.getChannel();
+
+                // Set our client brand
+                if (id.equals(CustomPayloadC2SPacket.BRAND)) {
+                    packet.setData(
+                        new PacketByteBuf(Unpooled.buffer()).writeString(
+                            this.getClientName()
+                        )
+                    );
+
+                    break;
+                }
+
+                // Cancel all packets to do with fabric
+                if (StringUtils.containsIgnoreCase(packet.getData().toString(StandardCharsets.UTF_8), "fabric")) {
+                    e.ci.cancel();
+
+                    break;
+                }
 
                 break;
             }
