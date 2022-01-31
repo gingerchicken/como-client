5a28186b2fa35c44cdb50ff9f8d1bee90facdd04
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-01-31 14:35:07 +0000

    Resolved #41

--- src/main/java/net/como/client/modules/utilities/NoBreak.java
+++ src/main/java/net/como/client/modules/utilities/NoBreak.java
@@ -2,6 +2,7 @@ package net.como.client.modules.utilities;
 
 import net.como.client.ComoClient;
 import net.como.client.events.SendPacketEvent;
+import net.como.client.events.UpdateBlockBreakingProgressEvent;
 import net.como.client.structures.Module;
 import net.como.client.structures.events.Event;
 import net.como.client.structures.settings.Setting;
@@ -23,16 +24,59 @@ public class NoBreak extends Module {
     @Override
     public void activate() {
         this.addListen(SendPacketEvent.class);
+        this.addListen(UpdateBlockBreakingProgressEvent.class);
     }
 
     @Override
     public void deactivate() {
         this.removeListen(SendPacketEvent.class);
+        this.removeListen(UpdateBlockBreakingProgressEvent.class);
+    }
+
+    private boolean shouldStopBreak() {
+        // Get the current item in the player's hand
+        ItemStack usedItem = ComoClient.me().getMainHandStack();
+
+        // Ignore if it is not a mining tool.
+        if (!(usedItem.getItem() instanceof net.minecraft.item.MiningToolItem)) return false;
+
+        // Get the current durability of the item.
+        Integer currentDurability = usedItem.getMaxDamage() - usedItem.getDamage();
+
+        // Get the minimum durability
+        Integer minDurability = (Integer)this.getSetting("MinDurability").value;
+
+        // If the item is still sufficiently durable still break the block.
+        if (currentDurability > minDurability) return false;
+
+        return true;
+    }
+
+    private Double lastMessageTime = 0d;
+    private void sendWarning() {
+        Double curTime = ComoClient.getCurrentTime();
+
+        if (lastMessageTime + 1 > curTime) return;
+        this.lastMessageTime = curTime;
+
+        this.displayMessage(String.format("Breaking prevented by NoBreak (less than %d durability met on active tool.)", this.getIntSetting("MinDurability")));
     }
 
     @Override
     public void fireEvent(Event event) {
         switch (event.getClass().getSimpleName()) {
+            case "UpdateBlockBreakingProgressEvent": {
+                UpdateBlockBreakingProgressEvent e = (UpdateBlockBreakingProgressEvent)event;
+
+                if (!this.shouldStopBreak()) break;
+
+                e.cir.cancel();
+
+                this.sendWarning();
+
+                break;
+            }
+
             case "SendPacketEvent": {
                 SendPacketEvent e = (SendPacketEvent)event;
 
@@ -42,23 +86,10 @@ public class NoBreak extends Module {
 
                 switch (actionPacket.getAction()) {
                     case STOP_DESTROY_BLOCK: {
-                        // Get the current item in the player's hand
-                        ItemStack usedItem = ComoClient.me().getMainHandStack();
-
-                        // Ignore if it is not a mining tool.
-                        if (!(usedItem.getItem() instanceof net.minecraft.item.MiningToolItem)) break;
-
-                        // Get the current durability of the item.
-                        Integer currentDurability = usedItem.getMaxDamage() - usedItem.getDamage();
-
-                        // Get the minimum durability
-                        Integer minDurability = (Integer)this.getSetting("MinDurability").value;
-
-                        // If the item is still sufficiently durable still break the block.
-                        if (currentDurability > minDurability) break;
+                        if (!this.shouldStopBreak()) break;
 
                         // Make sure to tell the user what we are doing.
-                        this.displayMessage(String.format("Breaking prevented by NoBreak (less than %d durability met on active tool.)", minDurability));
+                        this.sendWarning();
 
                         // We just need to tell the server that we actually stopped mining the block
                         PlayerActionC2SPacket stopPacket = new PlayerActionC2SPacket(Action.ABORT_DESTROY_BLOCK, actionPacket.getPos(), actionPacket.getDirection());
