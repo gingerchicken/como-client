a29753ad71e4856330dbf45bfedba97eb0ad8741
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-01-31 14:07:08 +0000

    Added UpdateBlockBreakingProgressEvent
    for #171

--- src/main/java/net/como/client/mixin/client/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/net/como/client/mixin/client/ClientPlayerInteractionManagerMixin.java
@@ -4,13 +4,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.como.client.ComoClient;
 import net.como.client.events.OnAttackEntityEvent;
 import net.como.client.events.StopUsingItemEvent;
+import net.como.client.events.UpdateBlockBreakingProgressEvent;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public class ClientPlayerInteractionManagerMixin {
@@ -22,5 +26,10 @@ public class ClientPlayerInteractionManagerMixin {
     @Inject(at = @At("HEAD"), method = "stopUsingItem(Lnet/minecraft/entity/player/PlayerEntity;)V", cancellable = true)
     private void onStopUsingItem(PlayerEntity playerEntity, CallbackInfo ci) {
         ComoClient.emitter.triggerEvent(new StopUsingItemEvent(playerEntity, ci));
+    }
+
+    @Inject(at = @At("HEAD"), method = "updateBlockBreakingProgress(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)Z", cancellable = true)
+    private void onUpdateBlockBreakingProgress(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
+        ComoClient.emitter.triggerEvent(new UpdateBlockBreakingProgressEvent(pos, direction, cir));
     }
 }
