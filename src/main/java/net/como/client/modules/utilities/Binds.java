5e10a2415fc8f10d3f6c34196d1c771bb752b860
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-02-13 23:35:28 +0000

    Merge binds commands
    The binds commands are now in the binds module

--- src/main/java/net/como/client/modules/utilities/Binds.java
+++ src/main/java/net/como/client/modules/utilities/Binds.java
@@ -2,6 +2,7 @@ package net.como.client.modules.utilities;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -10,7 +11,7 @@ import com.google.gson.Gson;
 
 import net.como.client.ComoClient;
 import net.como.client.GeneralConfig;
-import net.como.client.commands.BindsCommand;
+import net.como.client.commands.structures.Command;
 import net.como.client.events.OnKeyEvent;
 import net.como.client.structures.Module;
 import net.como.client.structures.events.Event;
@@ -155,7 +156,6 @@ public class Binds extends Module {
         this.addSetting(new Setting("HideCommandOutput", true));
         this.addSetting(new Setting("GUIKey", true));
 
-        ComoClient.commandHandler.registerCommand(new BindsCommand());
         this.setCategory("Utilities");
     }
 
@@ -259,5 +259,167 @@ public class Binds extends Module {
         data.put("binds", gson.toJson(flatBindsList));
 
         return data;
+    }
+
+    @Override
+    public Iterable<Command> getCommands() {
+        List<Command> commands = new ArrayList<>();
+
+        commands.add(new AddBind());
+        commands.add(new RemoveBind());
+        commands.add(new LogNext());
+
+        return commands;
+    }
+
+    private static class BindsSub extends Command {
+        public BindsSub(String command, String helpText, String description) {
+            super(command, helpText, description);
+        }
+
+        @Override
+        public boolean shouldShowHelp(String[] args) {
+            if (args.length < 2) return true;
+
+            return false;
+        }
+
+        public Binds getBinds() {
+            return (Binds)ComoClient.Modules.get("binds");
+        }
+
+        public boolean shouldDisplayKey(Integer keyCode) {
+            char c = this.fromKeyCode(keyCode);
+
+            return (Character.isLetterOrDigit(c));
+        }
+
+        public char fromKeyCode(Integer keyCode) {
+            return Character.toUpperCase((char)(int)keyCode);
+        }
+
+        public String keyBindString(Integer keyCode) {
+            return this.shouldDisplayKey(keyCode) ? String.format("%c", this.fromKeyCode(keyCode)) : String.format("0x%X", keyCode & 0xFFFFF);
+        }
+
+        public String getCommandArg(String[] args) {
+            return String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+        }
+
+        public Integer getKeyArg(String[] args) {
+            String arg = args[0];
+
+            // What if they gave a letter
+            if (arg.length() == 1) {
+                char c = arg.charAt(0);
+
+                // Make sure that it is a letter
+                if (Character.isLetter(c)) {
+                    // Get the uppercase since this is what is going to be caught by minecraft key.
+                    c = Character.toUpperCase(c);
+
+                    return (int)(c);
+                }
+            }
+
+            try {
+                return Integer.valueOf(arg);
+            } catch (Exception e) {
+                return -1;
+            }
+        }
+
+        public Boolean checkKey(int key) {
+            if (key == -1) {
+                this.getBinds().displayMessage(String.format("%sInvalid key, make sure it is the right key.", ChatUtils.RED));
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+    private static class AddBind extends BindsSub {
+        public AddBind() {
+            super("add", "bind add <key code|character> <command (without dot e.g. \"flight\")>", "Make a new key binding");
+        }
+
+        @Override
+        public Boolean trigger(String[] args) {
+            if (this.handleHelp(args)) return true;
+
+            int key = this.getKeyArg(args);
+            String command = this.getCommandArg(args);
+
+            if (checkKey(key)) return true;
+
+            if (!this.getBinds().addBind(key, command)) {
+                this.getBinds().displayMessage(String.format("%sFailed to add key bind.", ChatUtils.RED));
+                return true;
+            }
+
+            this.getBinds().displayMessage(String.format("Bound %s to run \"%s\".", this.keyBindString(key), command));
+            return true;
+        }
+    }
+    private static class LogNext extends BindsSub {
+        public LogNext() {
+            super("lognext", "", "Prints the next key's code in chat.");
+        }
+
+        @Override
+        public Boolean trigger(String[] args) {
+            this.getBinds().logNextKey = true;
+
+            this.getBinds().displayMessage("Press any key...");
+
+            return true;
+        }
+    }
+
+    // TODO add a list binds command
+
+    private static class RemoveBind extends BindsSub {
+        public RemoveBind() {
+            super("remove", "<key id|character> <optional specific command>", "Removes a bind from the list of binds");
+        }
+
+        @Override
+        public boolean shouldShowHelp(String[] args) {
+            return (args.length == 0);
+        }
+
+        @Override
+        public Boolean trigger(String[] args) {
+            if (this.handleHelp(args)) return true;
+
+            int key = this.getKeyArg(args);
+
+            if (checkKey(key)) return true;
+
+            // Handle entire key
+            if (args.length == 1) {
+                if (!this.getBinds().removeBind(key)) {
+                    this.getBinds().displayMessage(String.format("%sFailed to remove key bind for key '%s'.", ChatUtils.RED, this.keyBindString(key)));
+                    return true;
+                }
+
+                this.getBinds().displayMessage(String.format("Removed key bind '%s'.", this.keyBindString(key)));
+                return true;
+            }
+
+            String command = this.getCommandArg(args);
+
+            // Handle key + command
+            if (!this.getBinds().removeBind(key, command)) {
+                this.getBinds().displayMessage(String.format("%sFailed to remove key bind for key '%s' with command '%s'.", ChatUtils.RED, this.keyBindString(key), command));
+                return true;
+            }
+
+            this.getBinds().displayMessage(String.format("Removed bind '%s' from key '%s'.", command, this.keyBindString(key)));
+
+            return true;
+
+        }
     }
 }
