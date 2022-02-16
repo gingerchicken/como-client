179255b26e58d8e190985aa311800f92c3860acc
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-02-16 01:49:19 +0000

    Add ENCHANTMENTS attribute
    I feel this makes life a little easier

--- src/main/java/net/como/client/utils/NbtUtils.java
+++ src/main/java/net/como/client/utils/NbtUtils.java
@@ -1,6 +1,8 @@
 package net.como.client.utils;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtShort;
 import net.minecraft.nbt.NbtString;
 
@@ -13,4 +15,6 @@ public class NbtUtils {
 
         return enchant;
     }
+
+    public static final String[] ENCHANTMENTS = {"minecraft:protection","minecraft:fire_protection","minecraft:feather_falling","minecraft:blast_protection","minecraft:projectile_protection","minecraft:respiration","minecraft:aqua_affinity","minecraft:thorns","minecraft:depth_strider","minecraft:sharpness","minecraft:smite","minecraft:bane_of_arthropods","minecraft:knockback","minecraft:fire_aspect","minecraft:looting","minecraft:efficiency","minecraft:silk_touch","minecraft:unbreaking","minecraft:fortune","minecraft:power","minecraft:punch","minecraft:flame","minecraft:infinity","minecraft:luck_of_the_sea","minecraft:lure"};
 }
