65b0b131eed655e098db9ef94460187fe547c373
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-02-17 01:20:10 +0000

    Move MaxEnchant command

--- src/main/java/net/como/client/commands/nbt/NbtCommand.java
+++ src/main/java/net/como/client/commands/nbt/NbtCommand.java
@@ -10,5 +10,28 @@ public class NbtCommand extends CommandNode {
         this.addSubCommand(new MaxEnchant());
     }
 
+    public static class MaxEnchant extends Command {
 
+        public MaxEnchant() {
+            super("maxenchant", "", "Gives the current item every enchantment");
+        }
+    
+        @Override
+        public Boolean trigger(String[] args) {
+            ItemStack itemStack = ComoClient.me().getMainHandStack();
+    
+            NbtCompound nbt = new NbtCompound();
+            NbtList enchants = new NbtList();
+    
+            for (String id : NbtUtils.ENCHANTMENTS) {
+                enchants.add(NbtUtils.enchantment(id, Short.MAX_VALUE));
+            }
+    
+            nbt.put("Enchantments", enchants);
+    
+            itemStack.setNbt(nbt);
+    
+            return true;
+        }
+    }
 }
