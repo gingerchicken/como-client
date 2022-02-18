a520d61309221580fec890b0ad889268c83be06b
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-02-18 00:09:32 +0000

    Add packetsniffer
    This displays outbound packets

---
+++ src/main/java/net/como/client/modules/utilities/PacketSniffer.java
@@ -0,0 +1,70 @@
+package net.como.client.modules.utilities;
+
+import java.util.HashMap;
+
+import net.como.client.ComoClient;
+import net.como.client.events.SendPacketEvent;
+import net.como.client.structures.Module;
+import net.como.client.structures.events.Event;
+import net.como.client.structures.settings.Setting;
+import net.minecraft.network.Packet;
+import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
+
+public class PacketSniffer extends Module {
+    private Integer capturedPackets = 0;
+    private Double startTime = ComoClient.getCurrentTime();
+
+    public PacketSniffer() {
+        super("PacketSniffer");
+        
+        // TODO add inbound packets
+        this.description = "Displays captured outbound packets in chat";
+
+        this.addSetting(new Setting("HidePacket", new HashMap<String, Boolean>()));
+
+        this.setCategory("Utilities");
+    }
+    
+    @Override
+    public String listOption() {
+        return this.capturedPackets.toString();
+    }
+
+    @Override
+    public void activate() {
+        this.capturedPackets = 0;
+        startTime = ComoClient.getCurrentTime();
+    
+        this.addListen(SendPacketEvent.class);
+    }
+
+    @Override
+    public void deactivate() {
+        this.removeListen(SendPacketEvent.class);
+    }
+
+    private void displayPacket(Packet<?> packet) {
+        this.displayMessage(String.format("Captured (%f) %s", ComoClient.getCurrentTime() - this.startTime, packet.getClass().getSimpleName()));
+        this.capturedPackets++;
+    }
+
+    @Override
+    public void fireEvent(Event event) {
+        switch (event.getClass().getSimpleName()) {
+            case "SendPacketEvent": {
+                SendPacketEvent e = (SendPacketEvent)event;
+
+                Packet<?> packet = e.packet;
+                if (this.getHashMapSetting("HidePacket").containsKey(packet.getClass().getSimpleName())) break;
+
+                this.displayPacket(packet);
+
+                if (packet instanceof CreativeInventoryActionC2SPacket) {
+                    break;
+                }
+
+                break;
+            }
+        }
+    }
+}
