0c331552c797d1067f3c0cc5af5bec094a09e5ac
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-02-25 20:57:55 +0000

    Tweak NoItemRender
    Removes the entities rather than just hides them

---
+++ src/main/java/net/como/client/modules/render/NoItemRender.java
@@ -0,0 +1,51 @@
+package net.como.client.modules.render;
+
+import net.como.client.structures.events.Event;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.como.client.ComoClient;
+import net.como.client.events.ClientTickEvent;
+import net.como.client.structures.Module;
+
+public class NoItemRender extends Module {
+    public NoItemRender() {
+        super("NoItemRender");
+
+        this.description = "Hide all dropped items so then your friends cannot kill your client repeatedly.";
+        this.setCategory("Render");
+    }
+
+    @Override
+    public void activate() {
+        this.addListen(ClientTickEvent.class);
+    }
+
+    @Override
+    public void deactivate() {
+        this.removeListen(ClientTickEvent.class);
+    }
+
+    public void fireEvent(Event event) {
+        switch (event.getClass().getSimpleName()) {
+            case "ClientTickEvent": {
+                List<Entity> items = new ArrayList<>();
+
+                // Get all the entities
+                for (Entity entity : ComoClient.getClient().world.getEntities()) {
+                    if (!(entity instanceof ItemEntity)) continue;
+
+                    items.add(entity);
+                }
+
+                // Delete all
+                for (Entity entity : items) entity.discard();
+
+                break;
+            }
+        }
+    }
+}
