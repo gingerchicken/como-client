64282dd513a4c14e7823519ceb1fe0bdc15ed41f
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-02-26 10:59:37 +0000

    Add CrashSack
    This is a bundle that contains a lot of items!

---
+++ src/main/java/net/como/client/items/CrashSack.java
@@ -0,0 +1,34 @@
+package net.como.client.items;
+
+import net.como.client.structures.CreativeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtShort;
+import net.minecraft.nbt.NbtString;
+
+public class CrashSack implements CreativeItem {
+
+    @Override
+    public ItemStack getStack() {
+        ItemStack stack = new ItemStack(Items.BUNDLE);
+
+        NbtCompound nbt = new NbtCompound();
+        NbtList items = new NbtList();
+
+        NbtCompound item = new NbtCompound();
+        item.put("Count", NbtShort.of((short)64));
+        item.put("id", NbtString.of("minecraft:grass_block"));
+
+        for (int i = 0; i < Short.MAX_VALUE / 4; i++) {
+            items.add(item);
+        }
+
+        nbt.put("Items", items);
+
+        stack.setNbt(nbt);
+
+        return stack;
+    }
+}
