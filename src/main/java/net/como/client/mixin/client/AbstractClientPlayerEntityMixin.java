140496690f0595dce7860a7ee02f47a2042eec9b
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-03-13 16:57:56 +0000

    Add GetSkinTextureEvent
    This can be used to override a skin

---
+++ src/main/java/net/como/client/mixin/client/AbstractClientPlayerEntityMixin.java
@@ -0,0 +1,19 @@
+package net.como.client.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.como.client.ComoClient;
+import net.como.client.events.GetSkinTextureEvent;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.util.Identifier;
+
+@Mixin(AbstractClientPlayerEntity.class)
+public class AbstractClientPlayerEntityMixin {
+    @Inject(at = @At("RETURN"), method = "getSkinTexture()Lnet/minecraft/util/Identifier;", cancellable = true)
+    private void onGetSkinTexture(CallbackInfoReturnable<Identifier> cir) {
+        ComoClient.emitter.triggerEvent(new GetSkinTextureEvent((AbstractClientPlayerEntity)(Object)(this), cir));
+    }
+}
