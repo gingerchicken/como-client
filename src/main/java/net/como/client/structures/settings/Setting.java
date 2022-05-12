7bfa5a7976ccbdcf58f4e1ab3cafb815f960e7bd
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-05-12 23:31:24 +0100

    Add tooltip + description

--- src/main/java/net/como/client/structures/settings/Setting.java
+++ src/main/java/net/como/client/structures/settings/Setting.java
@@ -9,6 +9,27 @@ public class Setting {
 
     public final String name;
 
+    /**
+     * Describes what the setting is used for.
+     */
+    private String description = "";
+
+    /**
+     * Gets the description of the setting
+     * @return the description of the setting
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the description of the setting
+     * @param description the description of the setting
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     private List<String> suggestions = new ArrayList<>();
 
     public List<String> getSuggestions() {
@@ -29,5 +50,15 @@ public class Setting {
 
         this.value = defaultValue;
         this.suggestions.add(defaultValue.toString());
+    }
+
+    /**
+     * Gets a menu tool tip for a given setting
+     * @return
+     */
+    public String getToolTip() {
+        return this.getDescription().isBlank() 
+            ? String.format("No Description. (default: %s)", this.defaultValue.toString())
+            : String.format("%s. (default: %s)", this.getDescription(), this.defaultValue.toString());
     }
 }
