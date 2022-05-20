bb3e552cd4ab9682b36549c52b31960137611063
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-05-20 12:49:25 +0100

    Add fromHealth function

--- src/main/java/net/como/client/structures/Colour.java
+++ src/main/java/net/como/client/structures/Colour.java
@@ -2,6 +2,7 @@ package net.como.client.structures;
 
 import net.como.client.ComoClient;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 
 public class Colour {
     public float r,g,b,a;
@@ -24,6 +25,12 @@ public class Colour {
     public static Colour fromDistance(float distance) {
         float f = distance / 20F;
         return new Colour((2 - f) * 255, f*255, 0, 255/2);
+    }
+
+    public static Colour fromHealth(LivingEntity entity) {
+        float f = (entity.getHealth() / entity.getMaxHealth()) * 255*2;
+        
+        return new Colour((255*2 - f), (f), 0, 255);
     }
 
     public static Colour fromDistance(Entity entity) {
