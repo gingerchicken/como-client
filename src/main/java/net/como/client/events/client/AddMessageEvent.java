66c7149544cb8d14ba8b831814134e87ca57e483
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-09-03 16:38:04 +0100

    Update mixins to match new mappings

--- src/main/java/net/como/client/events/client/AddMessageEvent.java
+++ src/main/java/net/como/client/events/client/AddMessageEvent.java
@@ -7,12 +7,10 @@ import net.minecraft.text.Text;
 
 public class AddMessageEvent extends Event {
     public Text chatText;
-    public int chatLineId;
     public CallbackInfo ci;
 
-    public AddMessageEvent(Text chatText, int chatLineId, CallbackInfo ci) {
+    public AddMessageEvent(Text chatText, CallbackInfo ci) {
         this.chatText = chatText;
-        this.chatLineId = chatLineId;
         this.ci = ci;
     }
 }
