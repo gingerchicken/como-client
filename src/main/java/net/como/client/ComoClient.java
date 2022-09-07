00935563525d3142be5394ff5974c1a26788435a
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2022-09-07 02:19:33 +0100

    Add `discardlocal` command

--- src/main/java/net/como/client/ComoClient.java
+++ src/main/java/net/como/client/ComoClient.java
@@ -13,6 +13,7 @@ import net.como.client.commands.FontCommand;
 import net.como.client.commands.FriendsCommand;
 import net.como.client.commands.PanicCommand;
 import net.como.client.commands.WaypointsCommand;
+import net.como.client.commands.exploits.DiscardLocal;
 import net.como.client.commands.exploits.ParticleCrash;
 import net.como.client.commands.nbt.GiveCommand;
 import net.como.client.commands.nbt.NbtCommand;
@@ -130,6 +131,7 @@ public class ComoClient {
 
         // Exploit commands
         this.commandHandler.registerCommand(new ParticleCrash());
+        this.commandHandler.registerCommand(new DiscardLocal());
 
         // TODO replace this with just a Module sub command
         // Waypoints commands
